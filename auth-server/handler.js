c53f5ffe955ff033f3e72f818635e50f2608a077
Lo3z <carlosheins1@gmail.com>
2025-09-18 13:54:57 -0400

    Deployed serverless functions and added early functionality for authentication with a static test site

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv';
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 'use strict';
 
 import { google } from "googleapis";
-const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
-const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = ["https://meet-chi-five.vercel.app/"];
 
+const calendar = google.calendar("v3");
+const { CLIENT_ID, CLIENT_SECRET, CALENDAR_ID} = process.env;
 
 const oAuth2Client = new google.auth.OAuth2(
  CLIENT_ID,
@@ -13,8 +18,7 @@ const oAuth2Client = new google.auth.OAuth2(
  redirect_uris[0]
 );
 
-
-module.exports.getAuthURL = async () => {
+export async function getAuthURL() {
  const authUrl = oAuth2Client.generateAuthUrl({
    access_type: "offline",
    scope: SCOPES,
@@ -31,4 +35,73 @@ module.exports.getAuthURL = async () => {
      authUrl,
    }),
  };
-};
+}
+
+export async function getAccessToken(event) {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+}
+
+export async function getCalendarEvents(event) {
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+  .then ((results) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({ events: results.data.items }),
+    };
+  })
+  .catch ((error) => {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
+  });
+}
