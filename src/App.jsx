c4e802b0e371fdea57955073073211601ddfab0e
Lo3z <carlosheins1@gmail.com>
2025-09-23 18:29:50 -0400

    Troubleshooting problem with mock-data not displaying on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
 
 function App() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async() => {
+      const data = await getEvents();
+      console.log("fetched events:", data);
+      setEvents(data);
+    };
+    fetchEvents();
+  }, []);
+
   return (
       <div className="App">
         <CitySearch/>
-        <EventList/>
+        <EventList events={events}/>
         <NumberOfEvents/>
       </div>
   );
