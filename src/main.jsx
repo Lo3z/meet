7557615357f09af76f122a18cdf27c36664b56e0
Lo3z <carlosheins1@gmail.com>
2025-10-14 10:08:51 -0400

    Added service worker

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import * as atatus from 'atatus-spa';
 atatus.config('4a217011937f4dbbbe86a2202b806743').install();
 
-import * as serviceWorkerRegistration from '.serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
